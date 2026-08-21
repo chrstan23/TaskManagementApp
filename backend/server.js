9d5d955a01b5dd73dec5207d52947d5cca264b44
christian <alejandrechristian839@gmail.com>
2026-08-21 08:46:45 +0800

    connect backend to frontend (display all tasks to react)

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const cors = require("cors");
 const db = require("./db")
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 const PORT = 5000;
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,33 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [tasks, setTasks] = useState([]);
+
+    useEffect(() => {
+      fetch("http://localhost:5000/api/tasks")
+      .then((response) => response.json())
+      .then((data) => {
+        setTasks(data);
+      })
+      .catch((error) => {
+        console.error("Database error:", error);
+      });
+    }, []);
 
   return (
     <div>
+      <h1>Task Management</h1>
+
+      {tasks.map((task) => (
+        <div key={task.id}>
+          <h2>{task.title}</h2>
+          <p>{task.description}</p>
+          <p>Status: {task.status}</p>
+          <button>Update</button>
+          <button>Delete</button>
+        </div>
+      ))}
 
     </div>
   )
